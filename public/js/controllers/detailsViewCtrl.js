188b6b7bc4b8159570aa31513b5d148df29e681b
Oliver <oliviermedor@yahoo.com>
2015-03-04 12:10:18 -0500

    added module files

---
+++ public/js/controllers/detailsViewCtrl.js
@@ -0,0 +1,23 @@
+var app = angular.module('groupProject');
+
+app.directive('modalDialog', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      show: '='
+    },
+    replace: true, // Replace with the template below
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+      scope.dialogStyle = {};
+      if (attrs.width)
+        scope.dialogStyle.width = attrs.width;
+      if (attrs.height)
+        scope.dialogStyle.height = attrs.height;
+      scope.hideModal = function() {
+        scope.show = false;
+      };
+    },
+    template: '...' // See below
+  };
+});
