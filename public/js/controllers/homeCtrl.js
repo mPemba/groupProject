80c4173c71d2dc4fff4734aa155ab317e1ab183e
Michael Sayer <michael.j.say@gmail.com>
2015-03-04 19:52:44 -0700

    with photo/modal/and data on scope

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -2,6 +2,10 @@
 var app = angular.module('groupProject');
 
 app.controller('homeCtrl', function($scope, mapsService) {
+	$scope.modalShown = false;
+    $scope.toggleModal = function() {
+    $scope.modalShown = !$scope.modalShown;
+  };
 	$scope.test = "home page test test";
 
 	// $scope.getPlaces = function() {
@@ -11,6 +15,28 @@ app.controller('homeCtrl', function($scope, mapsService) {
 	// }
 
 	
-	mapsService.init();
+	// mapsService.init();
+
+
+
+	var newData = function(){
+    	
+		mapsService.init().then(function(res){
+			$scope.newDataArr = res;
+			var photo = res.photos;
+			console.log(photo)
+		    $scope.newPhoto = res[0].photos[0].getUrl({'maxWidth': 500, 'maxHeight': 500});
+		    console.log($scope.newPhoto)
+			// if (photo) {
+			// 	//$scope.newPhoto = photo.getUrl({'maxWidth': 500, 'maxHeight': 500}); 
+			// }
+
+		})
+
+	}
+	newData();
+	$scope.nextPlace = function(){
+
+	}
 
 })
