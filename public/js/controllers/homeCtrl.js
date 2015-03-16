2a45e7472aa072fbd023b81db075dba9fae2fe71
Oliver <oliviermedor@yahoo.com>
2015-03-16 13:17:55 -0400

    worked on the #map css, the homeCtrl and googelMapsService

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,31 +1,26 @@
-
 var app = angular.module('groupProject');
-
-app.controller('homeCtrl', function($scope, mapsService) {
-
+app.controller('homeCtrl', function($scope, $rootScope, mapsService) {
+	
 	$scope.modalShown = false;
     $scope.toggleModal = function() {
+    	newLocation($scope.newDataArr[counter - 1].geometry.location);
+    	console.log($scope.newDataArr[counter - 1].geometry.location);
     	$scope.getAllMapDetails();
     $scope.modalShown = !$scope.modalShown;
   };
-	$scope.test = "home page test test";
+  var newLocation = function(arg){
+		mapsService.changeMarkerPosition(arg);
 
+	}
+	$scope.test = "home page test test";
 	// $scope.getPlaces = function() {
 	// 	mapsService.getPlaces().then(function(res) {
 	// 		console.log(res);
 	// 	})
 	// }
-
-	
 	// mapsService.init();
-
-
-
 	var newData = function(){
-    	
 		mapsService.init().then(function(res){
-			
-
 			$scope.newDataArr = res;
 			
 			var photo = res.photos;
@@ -36,30 +31,41 @@ app.controller('homeCtrl', function($scope, mapsService) {
 			// 	//$scope.newPhoto = photo.getUrl({'maxWidth': 500, 'maxHeight': 500}); 
 			// }
 			$scope.nextPlace()
-
 		})
-
 	}
 	newData();
 	var counter = 0;
-
 	$scope.getAllMapDetails = function(){
-		console.log(counter)
 		mapsService.getAllDetails(counter).then(function(res){
 			$scope.phoneNumber = res.formatted_phone_number;
-			$scope.morePhotos = res.photos;
-			
-		});
-		
+			$scope.morePhotos = res.photos;			
+		});		
 	}
-	
 	$scope.nextPlace = function(){
 		$scope.newObjVar = $scope.newDataArr[counter];
 		counter = counter + 1;
-		
+	};
+$scope.clickAddInfo = function(){
+ 	//console.log($rootScope.user)
+	mapsService.postBusiness(
+		$rootScope.user,
+		$scope.comment,
+		$scope.newObjVar.name,
+		$scope.newObjVar.vicinity,
+		$scope.rating
+	)
+	$scope.comment = '';
+	$scope.rating = {
+        value: '1'
+      };
+};		
 
-
-
-	}
-
+// 	mapsService.getBusiness(
+// 		$scope.newObjVar.name,
+// 		$scope.newObjVar.vicinity
+// 	).then(function(res){
+// 		res.status(200).json(response);
+// 	}, function(err){
+// 		res.status(400).json(err);
+// 	})
 });
